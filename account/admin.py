321201bd5fb6129f0946240c689b4901ab31a681
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-07-28 09:32:48 +0500

    Asosiy baza uchun app yaratildi

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import User, Role
 
-# Register your models here.
+
+class RolesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'get_permissions']
+    list_display_links = ['id', 'name']
+
+    def get_permissions(self, obj):
+        return ", ".join([permission.name for permission in obj.permissions.all()])
+    get_permissions.short_description = "Boshqaruv"
+
+
+admin.site.register(Role, RolesAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_roles', 'username', 'first_name', 'last_name', 'created_at']
+    list_display_links = ['id', 'username', 'first_name', 'last_name']
+    search_fields = ['username', 'first_name']
+
+    def get_roles(self, obj):
+        return ", ".join([role.name for role in obj.role.all()])
+    get_roles.short_description = "Roles"
+
+
+admin.site.register(User, UserAdmin)
