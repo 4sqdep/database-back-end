a464311c0fd5554ff4b0fe26aff707894c08e129
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-07-28 10:36:21 +0500

    Asosiy baza kategoriyalari yaratildi

--- db_main/admin.py
+++ db_main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Categories, SubCategories
 
-# Register your models here.
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'user', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['id', 'name']
+
+
+admin.site.register(Categories, CategoriesAdmin)
+
+
+class SubCategoriesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'categories', 'parent', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['id', 'name']
+
+
+admin.site.register(SubCategories, SubCategoriesAdmin)
--- db_main/models.py
+++ db_main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+
+class Categories(models.Model):
+    """
+    Kategoriyalar uchun model
+    """
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Foydalanuvchi")
+    name = models.CharField(max_length=500, verbose_name="Nomi", blank=True, null=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Kiritilgan vaqti")
+
+    def __str__(self):
+        return f"{self.user} -- {self.name}"
+
+    class Meta:
+        verbose_name = "Kategoriya"
+        verbose_name_plural = "Kategoriyalar"
+
+
+class SubCategories(models.Model):
+    """
+    Pastgi Kategorialar uchun model
+    """
+    categories = models.ForeignKey(Categories, on_delete=models.SET_NULL, blank=True, null=True,
+                                   verbose_name="Kategoriyalar")
+    name = models.CharField(max_length=500, blank=True, null=True, verbose_name="Nomi", db_index=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True,
+                               verbose_name="Pastgi Kategoriyalar bolalari")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Kiritilgan vaqti")
+
+    def __str__(self):
+        return f"{self.categories} -- {self.name}"
+
+    class Meta:
+        verbose_name = "Pastgi Kategoriya"
+        verbose_name_plural = "Pastgi Kategoriyalar"
