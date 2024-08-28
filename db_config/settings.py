cac7933c9339693911accd3cd97ea54f99c4103f
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-08-28 11:55:43 +0500

    vercel test

--- db_config/settings.py
+++ db_config/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = "account.User"
 
