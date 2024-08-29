9ccf432d6842cc380c13e1d51820a1b83c0b843b
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-08-29 12:02:21 +0500

    Error file

--- db_config/urls.py
+++ db_config/urls.py
@@ -26,9 +26,10 @@ urlpatterns = [
         path('', include("account.urls")),
         path('main/', include("db_main.urls"))
     ]))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 urlpatterns += doc_urls
